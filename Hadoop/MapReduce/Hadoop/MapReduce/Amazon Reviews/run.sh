ef6adec477a6542c4d3b0b3d90f8de9bb6612a34
Samuel Carlos <samcarlos22@gmail.com>
2022-05-15 14:41:05 +0200

    Delete Hadoop/MapReduce directory

--- Hadoop/MapReduce/Hadoop/MapReduce/Amazon Reviews/run.sh
+++
@@ -1,4 +0,0 @@
-hadoop fs -rm -r hdfs:///{ path }
-hadoop jar hadoop-streaming-2.7.2.jar -D mapreduce.job.maps=20  -D mapreduce.job.reduce=2  -file mapper.py    -mapper mapper.py -file reducer.py   
--reducer reducer.py -input reviews.json -output debug
-hadoop fs -getmerge { path } output
